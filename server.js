1590522fe275dba42f66d587e87f13ffc1e2d2a5
David Celan <david.celan@mindthevalue.com>
2022-11-22 18:38:11 +0200

    working log in

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(`${__dirname}/views`))
+
+app.route('/').get((req,res)=>{
+   res.sendFile(path.join(`${__dirname}/views/index.html`));
+})
+
+app.listen(3001, ()=>console.log('started on 3001'));
+
+
+
+
+// app.listen(3000, "192.168.1.5", () => console.log("Server started")); //LAN HOST
+//instead of 192.168.1.5 should be LAN ip address
--- views/index.js
+++ views/index.js
@@ -1,9 +1,3 @@
-var rememberMeChecked = 0
-var rememberMeCheckbox = document.querySelector('#remember-me-checkbox')
-rememberMeCheckbox.addEventListener('change', ()=>{
-   this.checked ? rememberMeChecked=1 : rememberMeChecked=0;
-})
-
 if(window.localStorage.getItem('client') !== null){
    const client = JSON.parse(window.localStorage.getItem('client'));
    const username = document.querySelector('#username');
@@ -11,39 +5,51 @@ if(window.localStorage.getItem('client') !== null){
    var loginButton = document.querySelector('#login-button');
    loginButton.onclick = () => logout();
    loginButton.innerHTML = 'LOG OUT';
+   username.classList.add("disabled"); username.value = client.username;
+   password.classList.add("disabled"); password.value = client.password;
+}
 
-   username.classList.add('disabled');
-   password.classList.add('disabled');
+function wrongInput(obj){
+   obj.style.border = 'solid';
+   obj.style['border-color'] = 'red';
+   obj.style['background-color'] = 'rgb(231, 104, 104)';
+}
 
-   // if(rememberMeChecked === 1){
-      username.value = client.username;
-      password.value = password.username;
-   // }
+function resetInput(obj){
+   obj.style.border = 'none';
+   obj.style['background-color'] = 'rgb(206, 230, 168)';
 }
 
 function login(){
-   const username = document.querySelector('#username').value;
-   const password = document.querySelector('#password').value;
    
-   if(username === "" || password === ""){
-      if(username === ""){
-         username.style.border = 'solid';
-         username.style['border-color'] = 'red';
-         username.style['background-color'] = 'rgb(231, 104, 104)';
+   const username = document.querySelector('#username');
+   const password = document.querySelector('#password');
+   
+   if(username.value === "" && password.value === ""){
+      wrongInput(username); wrongInput(password);
+   }
+   else if(username.value === "") wrongInput(username);
+   else if(password.value === "") wrongInput(password);
+   else{
+      if(document.querySelector('#remember-me-checkbox').checked){
+         window.localStorage.setItem('client', `{"username": "${username.value}", "password": "${password.value}"}`)
       }
-      if(password === ""){
-         password.style.border = 'solid';
-         password.style['border-color'] = 'red';
-         password.style['background-color'] = 'rgb(231, 104, 104)';
-      }
-   }else{
-      window.localStorage.setItem('client', `{"username": "${username}", "password": "${password}"}`)
+      
       document.querySelector('#login-button').onclick=()=>logout();
       document.querySelector('#login-button').innerHTML = 'LOG OUT';
       document.querySelector('#username').classList.add('disabled');
       document.querySelector('#password').classList.add('disabled');
-   }
-   
+   }   
+}
+
+function logout(){
+   const username = document.querySelector("#username");
+   const password = document.querySelector("#password");
+   window.localStorage.clear();
+   document.querySelector('#login-button').onclick=()=>login();
+   document.querySelector('#login-button').innerHTML = 'LOG IN';
+   username.classList.remove('disabled');
+   password.classList.remove('disabled');
 }
 
 function showPass(){
