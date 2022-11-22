e605638b0a55c70b67a9b496cc25df7b2461184e
David Celan <david.celan@mindthevalue.com>
2022-11-22 17:11:04 +0200

    mogus

--- views/index.js
+++ views/index.js
@@ -9,20 +9,40 @@ if(window.localStorage.getItem('client') !== null){
    const username = document.querySelector('#username');
    const password = document.querySelector('#password');
    var loginButton = document.querySelector('#login-button');
-   loginButton.onclick = () => login();
+   loginButton.onclick = () => logout();
    loginButton.innerHTML = 'LOG OUT';
 
    username.classList.add('disabled');
    password.classList.add('disabled');
-   if(rememberMeChecked === 1){
+
+   // if(rememberMeChecked === 1){
       username.value = client.username;
       password.value = password.username;
-   }
+   // }
 }
 
 function login(){
    const username = document.querySelector('#username').value;
    const password = document.querySelector('#password').value;
+   
+   if(username === "" || password === ""){
+      if(username === ""){
+         username.style.border = 'solid';
+         username.style['border-color'] = 'red';
+         username.style['background-color'] = 'rgb(231, 104, 104)';
+      }
+      if(password === ""){
+         password.style.border = 'solid';
+         password.style['border-color'] = 'red';
+         password.style['background-color'] = 'rgb(231, 104, 104)';
+      }
+   }else{
+      window.localStorage.setItem('client', `{"username": "${username}", "password": "${password}"}`)
+      document.querySelector('#login-button').onclick=()=>logout();
+      document.querySelector('#login-button').innerHTML = 'LOG OUT';
+      document.querySelector('#username').classList.add('disabled');
+      document.querySelector('#password').classList.add('disabled');
+   }
    
 }
 
